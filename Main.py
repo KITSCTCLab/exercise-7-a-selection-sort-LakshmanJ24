e48c916b7931844d08d6b7511a4aadb7c0896ebb
LakshmanJ24 <100009458+LakshmanJ24@users.noreply.github.com>
2022-09-13 11:09:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ def selectionSort(array, size) -> List[int]:
   # Write your code here
   for i in range(0,size-2):
     min = i;
-    for j in range(i+1,n-1):
+    for j in range(i+1,size-1):
       if(array[j]<array[min]):
         min=j;
         array[i]=temp;
